c0a92b03809ed64a99d02089307738b546bdca77
Tomas O'Connell <tomas.oconnell9@mail.dcu.ie>
2026-03-02 14:38:18 +0000

    Progress on MPI_task function

--- week5/my_MPI_string_wave.c
+++ week5/my_MPI_string_wave.c
@@ -10,22 +10,72 @@ void update_positions(double* positions, int points, double time);
 int generate_timestamps(double* time_stamps, int time_steps, double step_size);
 double driver(double time);
 void print_header(FILE** p_out_file, int points);
-void MPI_task(int points, int cycles, int samples, char* output_path, int my_rank, int uni_size); //new function to perform the MPI task
+void MPI_task(int points, int cycles, int sample, char* output_path, int my_rank, int uni_size); //new function to perform the MPI task
 
 int main(int argc, char **argv)
 {
+	int points, cycles, sample;
+	char* output_path;
 
+	check_args(argc, argv, &points, &cycles, &sample, &output_path);
+
+	int ierror =0;
+	ierror = MPI_Init(&argc, &argv);
+
+	ierror = MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+	ierror = MPI_Comm_size(MPI_COMM_WORLD, &uni_size);
+
+	MPI_task(points, cycles, sample, output_path, my_rank, uni_size);
+
+	MPI_Finalize();
 
         return 0;
 }
 
 
-int MPI_task(int points, int cycles, int samples, char* output_path, int my_rank, int uni_size)
+int MPI_task(int points, int cycles, int sample, char* output_path, int my_rank, int uni_size)
 {
-	int time_steps = cycles * samples + 1;
+	int time_steps = cycles * sample + 1;
+	double step_size;
+
+	int chunk = points / uni_size; //how many points per process
+	int leftover = points % uni_size;
+	my_points = chunk; //each rank recieves a chunk
+
+	//allocate the leftover values to root task
+	if (my_rank == 0)
+	{
+		my_points += leftover;
+	}
+
+        // creates a vector for the time stamps in the data and generate them
+        double* time_stamps = (double*) malloc(time_steps * sizeof(double));
+        initialise_vector(time_stamps, time_steps, 0.0);
+        generate_timestamps(time_stamps, time_steps, step_size);
 
 
+	//split each process into their chunk
 
+	//for each chunk, update their position for each time_step
+
+	//for each MPI process send last value from its array to the next array since update_position needs i-1
+
+	//then we need to root to gather all the positions and write to the outfile
+
+
+	FILE* outfile;
+	if (my_rank ==0)
+	{
+		out_file = fopen(output_path, "w");
+		if (out_file == NULL)
+		{
+			fprintf(stderr, "ERROR, could not open this file");
+			exit(-1);
+		}
+		print_header(&out_file, points);
+	}
+
+	//
 }
 
 
